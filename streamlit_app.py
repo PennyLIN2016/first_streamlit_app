2669eada1c05be4470874cc4513ab635f320a6bb
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2023-07-25 14:15:51 +1000

    Update streamlit_app.py
    fix bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(my_fruit_list)
 # No selection
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Select avocado and strawberries
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 # filter 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
