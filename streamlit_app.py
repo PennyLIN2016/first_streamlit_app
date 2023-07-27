7adbb7e9f59680d238cdd947c3c1d72737d982a6
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2023-07-27 12:14:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+# Only display response code: 200
+#streamlit.text(fruityvice_response)
+# Display all json context
+streamlit.text(fruityvice_response.json())
 
 
 
