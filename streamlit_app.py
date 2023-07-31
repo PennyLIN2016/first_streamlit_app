8eb9c8fe031c3e602e45fec7c71359459e33bc22
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2023-07-31 14:35:21 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.dataframe(my_data_row)
 
-
+add_myfruit = 'test1'
 streamlit.write('Thanks for adding', add_myfruit)
 # this will not work correctly, but just go with it now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
